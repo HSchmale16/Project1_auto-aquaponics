79bee77fc710e57f33b504cdebf8e9f8f1465ac1
Henry J Schmale <hschmale16@gmail.com>
2016-12-19 14:17:44 -0500

    Add some comments

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,10 +1,16 @@
 #include <OneWire.h>
 
+/**
+ * Defines an action that the machine can take.
+ */
 struct CommandAction {
+    // The code used to look it up
     String code;
+    // use a function pointer to determine the action to take
     void (*action)();
 };
 
+// All possible actions that the machine can take.
 const CommandAction ACTIONS[] = {
     {"togPump", togglePump},
     {"readThm", readThermometer},
@@ -20,13 +26,16 @@ void setup() {
 
 void loop() {
     String cmd;
+    // while the serial is available append the current byte
     while(Serial.available()) {
+        // perform a chomp operation
         if(Serial.peek() == '\n'){
             Serial.read();
             break;
         }
         cmd += Serial.read(); 
     }
+    // Search through possible actions and execute that action on find
     for(byte i = 0; i < ACTION_COUNT; i++){
         if(cmd.equals(ACTIONS[i].code)) {
             ACTIONS[i].action();
@@ -36,17 +45,17 @@ void loop() {
 }
 
 void togglePump() {
-    
+    Serial.println(0);    
 }
 
 void readThermometer() {
-
+    Serial.println(0);
 }
 
 void readWaterLevel() {
-
+    Serial.println(0);
 }
 
 void toggleFeeder() {
-
+    Serial.println(0);
 }
