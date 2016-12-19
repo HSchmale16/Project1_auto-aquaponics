fa759ffd5301dcd69b0806e5a017dffd1581dae2
Henry J Schmale <hschmale16@gmail.com>
2016-12-18 22:54:06 -0500

    Start work on the arduino code

---
+++ Controller/Controller.ino
@@ -0,0 +1,50 @@
+struct CommandAction {
+    String code;
+    void (*action)();
+};
+
+const CommandAction ACTIONS[] = {
+    {"togPump", togglePump},
+    {"readThm", readThermometer},
+    {"readWLV", readWaterLevel},
+    {"togFeed", toggleFeeder}
+};
+const int ACTION_COUNT = sizeof(ACTIONS) / sizeof(CommandAction); 
+
+void setup() {
+    Serial.begin(54600);
+}
+
+
+void loop() {
+    String cmd;
+    while(Serial.available()) {
+        if(Serial.peek() == '\n'){
+            Serial.read();
+            break;
+        }
+        cmd += Serial.read(); 
+    }
+    for(byte i = 0; i < ACTION_COUNT; i++){
+        if(cmd.equals(ACTIONS[i].code)) {
+            ACTIONS[i].action();
+            break;
+        }
+    }
+}
+
+void togglePump() {
+    
+}
+
+void readThermometer() {
+
+}
+
+void readWaterLevel() {
+
+}
+
+void toggleFeeder() {
+
+}
--- sensord.js
+++ sensord.js
@@ -3,6 +3,14 @@
  */
 
 var SerialPort = require('serialport');
+var Readline = SerialPort.parsers.Readline;
+var config = require('config');
 
 var port = new SerialPort(config.serial.filename, {
     baudRate: config.serial.baudrate
+});
+var parser = port.pipe(new Readline());
+
+parser.on('data', console.log);
+
+
