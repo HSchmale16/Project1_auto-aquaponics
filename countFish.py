0fb0a8a9075fa94499ca80feda175678f02ba42f
Henry J Schmale <hschmale16@gmail.com>
2017-01-07 12:52:26 -0500

    Update bibliography

--- countFish.py
+++ countFish.py
@@ -4,4 +4,7 @@
 # Counts the number of fish in the aquaponics tank using blob detection
 # with OpenCV python bindings
 
+import cv2
+import numpy as np
 
+im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
