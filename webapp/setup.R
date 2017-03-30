80516a44604359c1a37fb7e3b3d31cb6a3e765ae
Henry J Schmale <hschmale16@gmail.com>
2017-03-30 16:19:08 -0400

    Add a setup script

---
+++ webapp/setup.R
@@ -0,0 +1,10 @@
+# setup.R
+
+install.packages('DT')
+install.packages('shiny')
+install.packages('shinydashboard')
+install.packages('plotly')
+install.packages('DBI')
+install.packages('dplyr')
+install.packages('ggplot2')
+install.packages('rjson')
